1d39e865fa4473ea426e95daa3bab2e217486501
Thomas Herrera <rayherrera90@outlook.com>
2019-03-04 13:27:54 -0700

    Added Entity classes.

---
+++ app/src/main/java/edu/cnm/deepdive/escaperoom/model/entity/Scenario.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.escaperoom.model.entity;
+
+import android.arch.persistence.room.Entity;
+
+@Entity(
+
+)
+
+public class Scenario {
+
+  private int scenario_id;
+
+  private String scenario_title;
+
+  private String media_picture;
+
+  private String accessible_text;
+
+}
